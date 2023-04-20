27471aff5cfc00ff0a89b10926e9a5d88a8c6780
rajeshkumar_iyyappan <rajeshkumar.iyyappan@gmail.com>
2023-04-20 11:40:59 +0530

    Added next js basic template

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          hef="/fonts/Montserrat-Bold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          hef="/fonts/Montserrat-ExtraBold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          hef="/fonts/Montserrat-Regular.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
